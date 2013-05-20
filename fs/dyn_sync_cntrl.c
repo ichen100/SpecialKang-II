a36ec62af801428b4e1c9da74510b5779b65014f
anarkia1976 <stefano.villa1976@gmail.com>
2013-05-20 06:59:13 +0200

    dyn_sync_cntrl.c: Enable Sync Globally --anarkia1976

--- fs/dyn_sync_cntrl.c
+++ fs/dyn_sync_cntrl.c
@@ -29,8 +29,8 @@
  */
 static DEFINE_MUTEX(fsync_mutex);
 
-bool early_suspend_active = false;
-static bool dyn_fsync_active = true;
+bool early_suspend_active __read_mostly = false;
+bool dyn_fsync_active __read_mostly = false;
 
 static ssize_t dyn_fsync_active_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
