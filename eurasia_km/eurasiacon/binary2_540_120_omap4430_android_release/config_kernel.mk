c3291600d8b575bf6aeaf46a623ac164eca2c72e
bsmitty <bsmitty83@yahoo.com>
2013-11-09 00:58:30 -0500

    add eurasia_km to kernel root This is the Imagination Technologies SGX DDK for the Linux kernel. For details see the "INSTALL" file.

---
+++ eurasia_km/eurasiacon/binary2_540_120_omap4430_android_release/config_kernel.mk
@@ -0,0 +1,7 @@
+override KERNELDIR := /home/bsmitty/android/smitty/SpecialKang-II/
+override KERNEL_COMPONENTS := srvkm bufferclass_example omaplfb_linux
+override KERNEL_CROSS_COMPILE := arm-none-linux-gnueabi-
+override PVR_SYSTEM := omap4
+override BUILD := release
+override LDM_PLATFORM := 1
+override OPTIM := -Os
